55a638a8fd36227a2226b1f6224087f7e0d30cff
zaneObwoni <zaneobwoni@gmail.com>
2024-05-30 20:55:45 +0200

    FIXED

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -3,7 +3,7 @@ import '../global.css'
 
 export default function RootLayout() {
     return (
-        <View className="bg-red-500 flex justify-center items-center flex-1">
+        <View className="bg-cyan-500 flex justify-center items-center flex-1">
             <Text>Settings page home!</Text>
         </View>
     )
