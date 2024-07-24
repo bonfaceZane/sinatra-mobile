f20a8d2fa6a8709118914e09557c3ad4b48834de
zaneObwoni <zaneobwoni@gmail.com>
2024-07-24 19:02:44 +0200

    everythings works

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -3,11 +3,8 @@ import React from "react";
 
 const Index = () => {
   return (
-    <View
-      className="flex-1 items-center justify-center w-screen
-	 h-screen"
-    >
-      <Text>Intro page!!! another one</Text>
+    <View className="flex-1 items-center justify-center w-full h-full bg-blue-600">
+      <Text className="text-[100px]">Intro page!!! sane</Text>
     </View>
   );
 };
