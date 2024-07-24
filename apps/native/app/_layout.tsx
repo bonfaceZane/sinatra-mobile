b2f2511443254b93940404a065a28efc8095d20a
zaneObwoni <zaneobwoni@gmail.com>
2024-07-24 18:17:30 +0200

    removed web app

---
+++ apps/native/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <View>
+            <Text>Intro page!!!</Text>
+        </View>
+    )
+}
+
+export default Index
